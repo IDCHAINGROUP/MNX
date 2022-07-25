2c6a71ee90e5272c127ab09bf1a572cb96ad328a
cryptodev0755 <66730985+cryptodev0755@users.noreply.github.com>
2022-07-25 15:30:18 +0530

    Minor bug fixed
    Top Bar UI bug Fixed

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -360,8 +360,14 @@ CAmount CMasternode::GetMNCollateral(int nHeight)
         return 15;
     } else if (nHeight <= 500000) {
         return 50;
-    } else {
+    } else if (nHeight <= 600000) {
         return 200;
+    } else if (nHeight <= 700000) {
+        return 300;
+    } else if (nHeight <= 1400000) {
+        return 3000;
+    } else {
+        return 15000;
     }
 
     return 0;
